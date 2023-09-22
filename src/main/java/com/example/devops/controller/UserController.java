97ebaccaa961e168932f0713a3b580e44c3da4c8
IlmaKhan04 <145204487+IlmaKhan04@users.noreply.github.com>
2023-09-22 17:49:38 +0530

    Update UserController.java

--- src/main/java/com/example/devops/controller/UserController.java
+++ src/main/java/com/example/devops/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.devops.controller;
 
-
 import com.example.devops.dto.UserDTO;
 import com.example.devops.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +14,33 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    // Bug: Missing error handling
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
-        UserDTO user = userService.getUserById(id);
-        if (user != null) {
-            return ResponseEntity.ok(user);
+        try {
+            UserDTO user = userService.getUserById(id);
+            if (user != null) {
+                return ResponseEntity.ok(user);
+            }
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            // Proper error handling is missing
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        return ResponseEntity.notFound().build();
     }
 
+    // Vulnerability: Inadequate input validation
     @PostMapping
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
-        UserDTO createdUser = userService.createUser(userDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        if (userDTO != null) {
+            UserDTO createdUser = userService.createUser(userDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        }
+        // Inadequate input validation; should return a proper error response
+        return ResponseEntity.badRequest().build();
     }
+
+    // Code Smell: Unused field
+    // @Autowired
+    // private UserService userService;
 }
